2e92dbf879d7fbe7542e21141cfc3a391923d527
alamin-cse44 <alamincse44@gmail.com>
2024-08-25 20:11:24 +0600

    Init aut app

---
+++ src/app/test/TestPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestPage = () => {
+    return (
+        <div>
+            <h1>Starting auth applciation!</h1>
+        </div>
+    );
+};
+
+export default TestPage;
