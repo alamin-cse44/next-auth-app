4707e1c82abb274cab02cb09d70866fa24fab9f6
alamin-cse44 <alamincse44@gmail.com>
2024-08-26 13:06:52 +0600

    add: initial providers

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,48 @@
 import nextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-const handler =  nextAuth({
-session: {
-    strategy: "jwt",   
-},
-providers: [],
-})
+const handler = nextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {
+          label: "Email address",
+          type: "email",
+          required: true,
+          placeholder: "Enter your email",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          required: true,
+          placeholder: "Enter your password",
+        },
+      },
+      async authorize(credentials) {
+        const { email, password } = credentials;
+        if (!credentials) {
+          return null;
+        }
+        if (email) {
+          const currentUser = users.find((user) => user.email === email);
+          if (currentUser) {
+            return currentUser;
+          }
+        }
 
-export { handler as GET, handler as POST};
+        return null;
+      },
+    }),
+  ],
+});
+
+const users = [
+  { id: 1, email: "a@gmail.com" },
+  { id: 2, email: "b@gmail.com" },
+  { id: 3, email: "c@gmail.com" },
+];
+
+export { handler as GET, handler as POST };
