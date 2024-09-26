2d7c84d741df9f93f72f93cbb23f7bd02469b683
alamin-cse44 <alamincse44@gmail.com>
2024-09-26 17:10:47 +0600

    Update: fetch product

--- src/app/(pages)/home/Product.jsx
+++ src/app/(pages)/home/Product.jsx
@@ -1,9 +1,11 @@
+"use client";
+
 import ProductsCard from "@/app/components/ProductsCard";
-import { getProducts } from "@/services/getProducts";
+import { useProductQuery } from "@/services/getProducts";
 
-
-const Product = async () => {
-  const data = await getProducts();
+const Product = () => {
+  // const data = await getProducts();
+  const { data: productItems, isLoading } = useProductQuery();
   return (
     <section className="py-16 container">
       <div className="text-center mb-12 flex flex-col gap-4">
@@ -15,11 +17,15 @@ const Product = async () => {
           Believable.
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data?.data?.map((product) => (
-          <ProductsCard key={product._id} product={product} />
-        ))}
-      </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {productItems.map((product) => (
+            <ProductsCard key={product._id} product={product} />
+          ))}
+        </div>
+      )}
       <div className="flex justify-center mt-8">
         <button className="btn btn-outline btn-primary">More Product</button>
       </div>
--- src/services/getProducts.js
+++ src/services/getProducts.js
@@ -1,3 +1,4 @@
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
 export const getProducts = async () => {
@@ -10,4 +11,20 @@ export const getProducts = async () => {
   } catch (error) {
     return [];
   }
+};
+
+
+const fetchProduct = async () => {
+  const res = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/dashboard/add-product/api/get-all`
+  );
+  return res?.data?.data;
+};
+
+export const useProductQuery = () => {
+  return useQuery({
+    queryKey: ["product"],
+    queryFn: fetchProduct,
+    staleTime: 0,
+  });
 };
